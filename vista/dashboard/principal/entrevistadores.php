aea58c778a8f857c2bc41c0f8e52ddc70386ba70
MatiasSalazar1029 <matiassalazar1029@gmail.com>
2024-05-28 15:09:15 +0200

    modificaciones en los estilos de agregar entrevistador

--- vista/dashboard/principal/entrevistadores.php
+++ vista/dashboard/principal/entrevistadores.php
@@ -170,22 +170,24 @@ if (!isset($_SESSION['nombre_sesion'])) {
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
+                                        <div class="card">
+                                            <div class="card-body">
                                         <form method="POST" action="../../../controlador/controlador-principal/controlador-agregarEntrevistadores.php" enctype="multipart/form-data">
                                             <div class="">
-                                                <label for="recipient-name" class="col-form-label">Nombre:</label>
-                                                <input type="text" class="form-control" id="nombre_entrevistador" name="nombre_entrevistador">
+                                            <div class="nombre-entrevistador" style="text-align:center; background-color:#CFE2FF; border: 1px solid #9ec5fe; margin-bottom: 5px;" ><label for="recipient-name" class="col-form-label">Nombre:</label></div>
+                                                <input type="text" class="form-control" id="nombre_entrevistador" name="nombre_entrevistador" style="margin-bottom: 5px;">
                                             </div>
                                             <div class="">
-                                                <label for="recipient-name" class="col-form-label">Apellido paterno:</label>
-                                                <input type="text" class="form-control" id="apellido_paterno_entrevistador" name="apellido_paterno_entrevistador">
+                                            <div class="nombre-entrevistador" style="text-align:center; background-color:#CFE2FF; border: 1px solid #9ec5fe; margin-bottom: 5px;" > <label for="recipient-name" class="col-form-label">Apellido paterno:</label></div>
+                                                <input type="text" class="form-control" id="apellido_paterno_entrevistador" name="apellido_paterno_entrevistador" style="margin-bottom: 5px;">
                                             </div>
                                             <div class="">
-                                                <label for="recipient-name" class="col-form-label">Apellido Materno:</label>
-                                                <input type="text" class="form-control" id="apellido_materno_entrevistador" name="apellido_materno_entrevistador">
+                                            <div class="nombre-entrevistador" style="text-align:center; background-color:#CFE2FF; border: 1px solid #9ec5fe; margin-bottom: 5px;" ><label for="recipient-name" class="col-form-label">Apellido Materno:</label></div>
+                                                <input type="text" class="form-control" id="apellido_materno_entrevistador" name="apellido_materno_entrevistador" style="margin-bottom: 5px;">
                                             </div>
                                             <div class="">
-                                                <label for="recipient-name" class="col-form-label">Sede</label>
-                                                <select name="sede" id="sede" class="form-control">
+                                            <div class="nombre-entrevistador" style="text-align:center; background-color:#CFE2FF; border: 1px solid #9ec5fe; margin-bottom: 5px;" ><label for="recipient-name" class="col-form-label">Sede</label></div>
+                                                <select name="sede" id="sede" class="form-select" style="margin-bottom: 5px;">
                                                     <option value="Imfca Contacto">Imfca Contacto</option>
                                                     <option value="JBG Operator">JBG Operator</option>
                                                     <option value="BKN">BKN</option>
@@ -197,6 +199,8 @@ if (!isset($_SESSION['nombre_sesion'])) {
                                                 <button type="submit" name="submit" class="btn btn-success">Registrar</button>
                                             </div>
                                         </form>
+                                        </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
